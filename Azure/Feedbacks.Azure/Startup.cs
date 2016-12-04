7bbc806b25ff7513a78f8670756acec456132b02
Denis Kretov <kretov.d@gmail.com>
2016-12-04 10:11:22 +0300

    FirstAzureCommit

---
+++ Azure/Feedbacks.Azure/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Feedbacks.Azure.Startup))]
+
+namespace Feedbacks.Azure
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
